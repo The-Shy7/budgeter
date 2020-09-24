01cebc057094652062dba783ce732dcc1e41e908
tahmit <tahmit@uw.edu>
2020-09-23 22:18:32 -0700

    added calender and report icons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { MdPictureInPicture, MdAssessment } from "react-icons/md";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="white-bar">
+          <div className="icon">
+            {<MdPictureInPicture size={32}/>}
+            Calendar
+            {<MdAssessment size={32}/>}
+            Report 
+          </div>
+        </div>
+        <span className="title">
+          Hello User!
+        </span>
+        {/* <Navbar /> */}
     </div>
   );
 }
+
+// class Navbar extends Component {
+//   render () {
+//       return (
+//           <div className = "navbar bg"> 
+//               <div className = "wrapper"> 
+//                   <h2 className = "title">
+//                       Hello User!
+//                   </h2>
+//               </div>
+//           </div>
+//       )
+//   }
+// }
 
 export default App;
